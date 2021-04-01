efb7634c8dd67b232a2b3332ad42d6186983434f
Hasan Moboarak <hsncool8@gmail.com>
2021-04-01 12:09:35 +0600

    acquisition chart component complete

--- packages/main/src/components/dashboard-two/acquisition/style.tsx
+++ packages/main/src/components/dashboard-two/acquisition/style.tsx
@@ -1,4 +1,4 @@
-import styled, { device } from "@doar/shared/styled";
+import styled, { device, themeGet, css } from "@doar/shared/styled";
 import { CardHeader } from "@doar/components";
 
 export const StyledHeader = styled(({ ...rest }) => <CardHeader {...rest} />)`
@@ -9,5 +9,79 @@ export const StyledHeader = styled(({ ...rest }) => <CardHeader {...rest} />)`
     ${device.large} {
         padding-top: 25px;
         padding-left: 25px;
+    }
+`;
+
+export const StyledTitle = styled.h6`
+    margin-bottom: 5px;
+`;
+
+export const StyledDesc = styled.p`
+    font-size: 12px;
+    color: #8392a5;
+    margin-bottom: 0px;
+`;
+
+interface IMediaIcon {
+    $variant: "primary" | "light";
+}
+
+export const StyledMediaIcon = styled.div<IMediaIcon>`
+    width: 40px;
+    opacity: 0.6;
+    height: 40px;
+    border-width: 2px;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+    display: flex;
+    border-radius: ${themeGet("radii.rounded")};
+    ${({ $variant }) =>
+        $variant === "primary" &&
+        css`
+            border: 2px solid ${themeGet("colors.primary")};
+            svg {
+                color: ${themeGet("colors.primary")};
+            }
+        `}
+    ${({ $variant }) =>
+        $variant === "light" &&
+        css`
+            border: 2px solid ${themeGet("colors.gray500")};
+            svg {
+                color: ${themeGet("colors.gray500")};
+            }
+        `}
+`;
+
+export const StyledMediaTitle = styled.h4`
+    line-height: 1.1;
+    letter-spacing: -1px;
+    font-family: ${themeGet("fonts.rubik")};
+    font-weight: 400;
+    margin-bottom: 5px;
+`;
+
+export const StyledMediaDesc = styled.p`
+    font-size: 10px;
+    white-space: nowrap;
+    text-transform: uppercase;
+    letter-spacing: 0.5px;
+    font-weight: 500;
+    color: ${themeGet("colors.text3")};
+    margin-bottom: 0px;
+`;
+
+export const StyledChart = styled.div`
+    height: 126px;
+    width: 100%;
+    ${device.large} {
+        height: 160px;
+        margin-bottom: -20px;
+        margin-top: -44px;
+        & > div {
+            margin-left: -20px;
+            margin-right: -8px;
+        }
     }
 `;
--- packages/shared/data/dashboard-two/index.ts
+++ packages/shared/data/dashboard-two/index.ts
@@ -1,4 +1,5 @@
 import webMetricsChart from "./web-metrics-chart";
 import sessionsChart from "./sessions-chart";
+import acquisitionChart from "./acquisition-chart";
 
-export { webMetricsChart, sessionsChart };
+export { webMetricsChart, sessionsChart, acquisitionChart };

e63db36e5bb9b8820206ba32b2a9aec2dea09fd6
hasanm95 <hsncool10@gmail.com>
2021-12-11 11:40:07 +0600

    updated to react-router-dom@v6

--- packages/main/src/App.tsx
+++ packages/main/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Preloader from "./components/preloader";
 
 const DashboardOne = lazy(() => import("./pages/dashboard-one"));
@@ -33,77 +33,51 @@ const App: React.FC = () => {
         <>
             <Router>
                 <Suspense fallback={<Preloader />}>
-                    <Switch>
-                        <Route exact path="/" component={DashboardOne} />
+                    <Routes>
+                        <Route path="/" element={<DashboardOne />} />
                         <Route
-                            exact
                             path="/dashboard-two"
-                            component={DashboardTwo}
+                            element={<DashboardTwo />}
                         />
                         <Route
-                            exact
                             path="/dashboard-three"
-                            component={DashboardThree}
+                            element={<DashboardThree />}
                         />
                         <Route
-                            exact
                             path="/dashboard-four"
-                            component={DashboardFour}
+                            element={<DashboardFour />}
                         />
-                        <Route exact path="/signin" component={SignIn} />
-                        <Route exact path="/signup" component={SignUp} />
+                        <Route path="/signin" element={<SignIn />} />
+                        <Route path="/signup" element={<SignUp />} />
                         <Route
-                            exact
                             path="/verify-account"
-                            component={VerifyAccount}
+                            element={<VerifyAccount />}
                         />
                         <Route
-                            exact
                             path="/forgot-password"
-                            component={ForgotPassword}
+                            element={<ForgotPassword />}
                         />
-                        <Route exact path="/error-500" component={Error500} />
-                        <Route exact path="/error-503" component={Error503} />
-                        <Route exact path="/error-505" component={Error505} />
+                        <Route path="/error-500" element={<Error500 />} />
+                        <Route path="/error-503" element={<Error503 />} />
+                        <Route path="/error-505" element={<Error505 />} />
+                        <Route path="/profile-view" element={<ProfileView />} />
+                        <Route path="/connections" element={<Connections />} />
+                        <Route path="/groups" element={<Groups />} />
+                        <Route path="/events" element={<Events />} />
+                        <Route path="/timeline" element={<Timeline />} />
+                        <Route path="/pricing" element={<Pricing />} />
+                        <Route path="/help-center" element={<HelpCenter />} />
+                        <Route path="/invoice" element={<Invoice />} />
+                        <Route path="/apps/calendar" element={<Calendar />} />
+                        <Route path="/apps/chat" element={<Chat />} />
+                        <Route path="/apps/contacts" element={<Contacts />} />
                         <Route
-                            exact
-                            path="/profile-view"
-                            component={ProfileView}
-                        />
-                        <Route
-                            exact
-                            path="/connections"
-                            component={Connections}
-                        />
-                        <Route exact path="/groups" component={Groups} />
-                        <Route exact path="/events" component={Events} />
-                        <Route exact path="/timeline" component={Timeline} />
-                        <Route exact path="/pricing" component={Pricing} />
-                        <Route
-                            exact
-                            path="/help-center"
-                            component={HelpCenter}
-                        />
-                        <Route exact path="/invoice" component={Invoice} />
-                        <Route
-                            exact
-                            path="/apps/calendar"
-                            component={Calendar}
-                        />
-                        <Route exact path="/apps/chat" component={Chat} />
-                        <Route
-                            exact
-                            path="/apps/contacts"
-                            component={Contacts}
-                        />
-                        <Route
-                            exact
                             path="/apps/file-manager"
-                            component={FileManager}
+                            element={<FileManager />}
                         />
-                        <Route exact path="/apps/mail" component={Mail} />
-                        <Route path="*" component={ErrorNotFound} />
-                    </Switch>
+                        <Route path="/apps/mail" element={<Mail />} />
+                        <Route path="*" element={<ErrorNotFound />} />
+                    </Routes>
                 </Suspense>
             </Router>
 

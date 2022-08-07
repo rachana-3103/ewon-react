d0badb04a7ecb33179407b711fa911548c5973a8
hasanm95 <hsncool10@gmail.com>
2022-08-07 09:59:15 +0600

    updated redux

--- packages/components/dist/src/maps/google-map/google-map.js
+++ packages/components/dist/src/maps/google-map/google-map.js
@@ -22,6 +22,7 @@ var __rest = (this && this.__rest) || function (s, e) {
 };
 import { jsx as _jsx } from "react/jsx-runtime";
 import { Wrapper } from "@googlemaps/react-wrapper";
+import { MapKey } from "@doar/shared/data";
 import Map from "./map";
 import Marker from "./marker";
 import { StyledMap } from "./style";
@@ -30,7 +31,7 @@ var render = function (status) {
 };
 var MyMap = function (_a) {
     var width = _a.width, height = _a.height, marker = _a.marker, options = __rest(_a, ["width", "height", "marker"]);
-    return (_jsx(StyledMap, __assign({ width: width, height: height }, { children: _jsx(Wrapper, __assign({ apiKey: "AIzaSyB3mMuvl8IUlviRZiizBiX7uhsdIqunx94", render: render }, { children: _jsx(Map, __assign({}, options, { children: marker && _jsx(Marker, { position: options.center }) })) })) })));
+    return (_jsx(StyledMap, __assign({ width: width, height: height }, { children: _jsx(Wrapper, __assign({ apiKey: MapKey, render: render }, { children: _jsx(Map, __assign({}, options, { children: marker && _jsx(Marker, { position: options.center }) })) })) })));
 };
 MyMap.defaultProps = {
     center: { lat: -3.745, lng: -38.523 },
